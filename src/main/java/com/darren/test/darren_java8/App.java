223dd9d6a7f89f9f4f7bc587ea4273adab005c54
Darren Zhang <sf0902@163.com>
2018-08-13 13:03:05 +0800

    initial java8 project

---
+++ src/main/java/com/darren/test/darren_java8/App.java
@@ -0,0 +1,13 @@
+package com.darren.test.darren_java8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
